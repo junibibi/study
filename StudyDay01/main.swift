ba23c088ce1aa2fdd7cbddeb01551537f0993703
JH <jh@172.20.nate.com>
2018-06-30 22:35:29 +0900

    for 수업

--- StudyDay01/main.swift
+++ StudyDay01/main.swift
@@ -1,12 +1,61 @@
+//for a in 1...9 {
+//    print(a)
+//}
 //
-//  main.swift
-//  StudyDay01
+//for b in 2...9{
+//    for c in 1...9{
+//    print(b*c)
+//    }
+//}
 //
-//  Created by JH on 2018. 6. 23..
-//  Copyright © 2018년 JH. All rights reserved.
 //
+//for d in 2...3{
+//    for e in 1...9{
+//        print("\(d)*\(e)=\(d*e)")
+//    }
+//}
+//
+//
+//    for f in 1...5{
+//        for g in 1...f+1{
+//            print(f*g)
+//
+//        }
+//    }
 
-import Foundation
 
-print("Hello, World!")
+//let v = 10 //별의 높이
+//var n = ""
+//
+//for x in 0..<v {
+//    
+//
+//    for _ in 0..<x + 1 {
+//        n = n + "*"
+//    }
+//    print(n)
+//    n = ""
+//}
 
+
+//let p = 10
+//var h = ""
+//
+//for j in 0..<p {
+//    for _ in 0..<p - j {
+//        h = h + "*"
+//    }
+//    print(h)
+//    h = ""
+//}
+
+//let p = 10
+//var h = ""
+//
+//for j in 0..<p {
+//    for _ in j..<p {
+//        h = h + "*"
+//    }
+//    print(h)
+//    h = ""
+//}
